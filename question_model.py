dcec31dd5c937353da56fc4e5cec67b023a17887
peter ngotho <sammykamau772@mail.com>
2021-06-15 05:41:00 -0400

    main class file implemented

--- question_model.py
+++ question_model.py
@@ -1,9 +1,5 @@
 class Question:
-    def __init__(self,q_text,q_answer):
+    def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
-
-
-new_q = Question("sdfsdf", "False")
-print(new_q)
 
